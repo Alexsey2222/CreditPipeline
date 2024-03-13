59bbc9dd048d65b04cdb918db5a9ac72890773ea
Alexey <a-stakanov@mail.ru>
2024-03-13 13:18:30 +0300

    Start

---
+++ src/test/java/com/example/CreditPipeline/CreditPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CreditPipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
